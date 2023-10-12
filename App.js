8c1ea8ddc1a49eb6f015c46ecdebbf0e5bac02dd
Jenni-Maria <t1kyje00@students.oamk.fi>
2023-10-12 18:25:26 +0300

    WeatherApp modifications

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather';
+import Position from './components/Position';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto'
+          backgroundColor='lightgreen'/>
+      <Weather />
+      <Position />
     </View>
   );
 }
@@ -16,5 +21,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight:0,
+    marginTop: 100,
+    padding: 20,
+    
   },
 });
---
+++ components/Position.js
@@ -0,0 +1,54 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import * as Location from './Weather';
+import Weather from './Weather';
+
+export default function Position() {
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    const [message, setMessage] = useState('Retrieving location...')
+    const [isLoading, setIsloading] = useState(true)
+
+useEffect(() => {
+    (async() => {
+        let {status} = await Location.requestForegroundPermissionsAsync()
+        console.log(status)
+        try {
+            if (status !== 'granted') {
+                setMessage("Location not permitted.")
+
+            } else {
+                const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
+                setLatitude(position.coords.latitude)
+                setLongitude(position.coords.longitude)
+                setMessage('Location retrieved')
+            }
+        } catch (error) {
+            setMessage("Error retrieving location")
+            console.log(error)
+        }
+        setIsloading(false)
+    })()
+}, [])
+
+  return (
+    <View>
+      <Text style={styles.coords}>{latitude.toFixed(3)},{longitude.toFixed(3)}</Text>
+
+        <Text style={styles.coords}>{message}</Text>
+        {isLoading === false &&
+        <Weather latitude={latitude} longitude={longitude} />
+}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    coords: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+  });
